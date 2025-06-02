a1917a503ea05d811a5cf647514d564bdd1580fa
DESKTOP-NH7VU9B\chris <christianharris0403@gmail.com>
2025-06-02 22:33:51 +0100

    added basic version of Crytter

---
+++ src/Crytter.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+
+var in_shell = true
+
+func process(player):
+	if position.distance_to(player.position) < 100:
+		get_out_of_shell()
+	else:
+		get_in_shell()
+
+
+func get_out_of_shell():
+	if in_shell:
+		in_shell = false
+		$Sprite.play("get_up")
+
+func get_in_shell():
+	if not in_shell:
+		in_shell = true
+		$Sprite.play_backwards("get_up")
