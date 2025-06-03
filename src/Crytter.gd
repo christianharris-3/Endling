0957a5d9b87dfcb285e2b26bb9ebd7ec90158f97
DESKTOP-NH7VU9B\chris <christianharris0403@gmail.com>
2025-06-03 15:58:54 +0100

    added a walking animation to the crytter

--- src/Crytter.gd
+++ src/Crytter.gd
@@ -3,8 +3,10 @@ extends CharacterBody2D
 var in_shell = true
 
 func process(player):
-	if position.distance_to(player.position) < 100:
+	if position.distance_to(player.position) < 500:
 		get_out_of_shell()
+		self.position.x-=1
+		$Sprite.play("walk")
 	else:
 		get_in_shell()
 
